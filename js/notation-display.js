a4b1788ce771a75083e43689796d5b0271ab71a0
Amy Y <ctrlalternativedelete@gmail.com>
2020-05-25 17:35:11 +1000

    vexflow

---
+++ js/notation-display.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+VF = Vex.Flow;
+
+// We created an object to store the information about the workspace
+var WorkspaceInformation = {
+    // The <canvas> element in which you're going to work
+    canvas: document.getElementById('some-canvas-id'),
+    // Vex creates a canvas with specific dimensions
+    canvasWidth: 500,
+    canvasHeight: 500
+};
+
+// Create a renderer with Canvas
+var renderer = new VF.Renderer(
+    WorkspaceInformation.canvas,
+    VF.Renderer.Backends.CANVAS
+);
+
+// Use the renderer to give the dimensions to the canvas
+renderer.resize(WorkspaceInformation.canvasWidth, WorkspaceInformation.canvasHeight);
+
+// Expose the context of the renderer
+var context = renderer.getContext();
+
+// And give some style to our canvas
+context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+
+
+/**
+ * Creating a new stave
+ */
+// Create a stave of width 400 at position x10, y40 on the canvas.
+var stave = new VF.Stave(10, 40, 400);
+// Add a clef and time signature.
+stave.addClef("treble").addTimeSignature("4/4");
+// Set the context of the stave our previous exposed context and execute the method draw !
+stave.setContext(context).draw();
+}
