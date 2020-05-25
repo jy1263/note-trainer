3755afec07594430e451ea678090e554e8beba90
Amy Y <ctrlalternativedelete@gmail.com>
2020-05-25 20:07:48 +1000

    added some messy rudementary code now works i guess

--- js/notation-display.js
+++ js/notation-display.js
@@ -1,38 +1,63 @@
-window.onload = function() {
-VF = Vex.Flow;
+window.onload = function(){
+    this.refresh(undefined);
+}
 
-// We created an object to store the information about the workspace
-var WorkspaceInformation = {
-    // The <canvas> element in which you're going to work
-    canvas: document.getElementById('some-canvas-id'),
-    // Vex creates a canvas with specific dimensions
-    canvasWidth: 500,
-    canvasHeight: 500
-};
+let i = 0;
+let randomchars = [];
 
-// Create a renderer with Canvas
-var renderer = new VF.Renderer(
-    WorkspaceInformation.canvas,
-    VF.Renderer.Backends.CANVAS
-);
+function refresh(e) {
+    VF = Vex.Flow;
 
-// Use the renderer to give the dimensions to the canvas
-renderer.resize(WorkspaceInformation.canvasWidth, WorkspaceInformation.canvasHeight);
+    if (randomchars[i - 1] == e ) {
+        console.log("correct")
+    
+    
+    var svg = document.querySelector("svg");
+    if (svg != null) {
+        svg.parentNode.removeChild(svg);
+    }
 
-// Expose the context of the renderer
-var context = renderer.getContext();
+    randomchar = String.fromCharCode(65+Math.floor(Math.random() * 7)).toString()
+    randomchars.push(randomchar)
+    var randomiser = [randomchar+ "/" + (Math.floor(Math.random() * 2) + 4).toString()];
 
-// And give some style to our canvas
-context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+    
 
 
-/**
- * Creating a new stave
- */
-// Create a stave of width 400 at position x10, y40 on the canvas.
-var stave = new VF.Stave(10, 40, 400);
-// Add a clef and time signature.
-stave.addClef("treble").addTimeSignature("4/4");
-// Set the context of the stave our previous exposed context and execute the method draw !
-stave.setContext(context).draw();
+    var notes = [
+        // A quarter-note C.
+        new VF.StaveNote({clef: "treble", keys: randomiser, duration: "q" }),
+    ];
+
+    // Create an SVG renderer and attach it to the DIV element named "notation".
+    var div = document.getElementById("notation")
+    var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+    
+    // Size our SVG:
+    renderer.resize(notes.length*100+20, 200);
+    
+    // And get a drawing context:
+    var context = renderer.getContext();
+
+    // Create a stave at position 10, 40 of width 400 on the canvas.
+    var stave = new VF.Stave(10, 20, notes.length*100);
+
+    // Add a clef and time signature.
+    stave.addClef("treble") //.addTimeSignature("3/4");
+
+    // Connect it to the rendering context and draw!
+    stave.setContext(context).draw();
+
+    // Create a voice in 4/4 and add the notes from above
+    var voice = new VF.Voice({num_beats: notes.length,  beat_value: 4});
+    voice.addTickables(notes);
+    
+    // Format and justify the notes to 400 pixels.
+    var formatter = new VF.Formatter().joinVoices([voice]).format([voice], notes.length*100);
+    
+    // Render voice
+    voice.draw(context, stave);
+
+    i++
+    }
 }
