1e8a017e927b4b3189b33092c1aaa4e47170950d
Amy Y <ctrlalternativedelete@gmail.com>
2020-05-26 21:10:23 +1000

    vue rewrite init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,94 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="centeralign" style="height: 80%;">
+      <div id="notation"></div>
+    </div>
+    <div class="centeralign" style="height: 20%;" id="bottomalign">
+      <button class="reroll" onclick='refreshlock("C")'>C</button>
+      <button class="reroll" onclick='refreshlock("D")'>D</button>
+      <button class="reroll" onclick='refreshlock("E")'>E</button>
+      <button class="reroll" onclick='refreshlock("F")'>F</button>
+      <button class="reroll" onclick='refreshlock("G")'>G</button>
+      <button class="reroll" onclick='refreshlock("A")'>A</button>
+      <button class="reroll" onclick='refreshlock("B")'>B</button>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  import Vex from 'vexflow/src/index.js'
+  let VF = Vex.Flow;
+
+  export default {
+    name: 'HelloWorld',
+
+    props: {
+      msg: String
+    },
+
+    data: function () {
+      return {
+        indexOfArray: 0,
+        randomchars: []
+      }
+    },
+
+    mounted () {
+      this.refresh();
+    },
+
+    methods: {
+      refresh: function() {
+        var svg = document.querySelector("svg");
+        if (svg != null) {
+            svg.parentNode.removeChild(svg);
+        }
+
+        let randomchar = String.fromCharCode(65+Math.floor(Math.random() * 7)).toString()
+        this.$data.randomchars.push(randomchar)
+        var randomiser = [randomchar+ "/" + (Math.floor(Math.random() * 2) + 4).toString()];
+
+        console.log(this.$data.randomchars)
+
+        var notes = [
+            // A quarter-note C.
+            new VF.StaveNote({clef: "treble", keys: randomiser, duration: "q" }),
+        ];
+
+        // Create an SVG renderer and attach it to the DIV element named "notation".
+        var div = document.getElementById("notation")
+        var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+        
+        // Size our SVG:
+        renderer.resize(notes.length*100+20, 200);
+        
+        // And get a drawing context:
+        var context = renderer.getContext();
+
+        // Create a stave at position 10, 40 of width 400 on the canvas.
+        var stave = new VF.Stave(10, 20, notes.length*100);
+
+        // Add a clef and time signature.
+        stave.addClef("treble") //.addTimeSignature("3/4");
+
+        // Connect it to the rendering context and draw!
+        stave.setContext(context).draw();
+
+        // Create a voice in 4/4 and add the notes from above
+        var voice = new VF.Voice({num_beats: notes.length,  beat_value: 4});
+        voice.addTickables(notes);
+        
+        // Format and justify the notes to 400 pixels.
+        var formatter = new VF.Formatter().joinVoices([voice]).format([voice], notes.length*100);
+        
+        // Render voice
+        voice.draw(context, stave);
+      }
+    } 
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+
 </style>
